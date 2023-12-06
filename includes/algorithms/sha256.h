7315fb701214c1c2e2e8ab458a461b34213cbf0a
Damien <hubleur.damien333@protonmail.ch>
2023-12-06 22:38:30 +0100

    semi working sha256

--- includes/algorithms/sha256.h
+++ includes/algorithms/sha256.h
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 21:58:19 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/12/06 22:16:37 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/12/06 22:29:09 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ void sha256_string(char *string, uint8_t *result);
 
 // Intern
 typedef struct {
-    uint64_t size;        // Use to accumulate the size of the message (in bytes)
-    uint32_t buffer[8];   // A/B/C/D/E/F/G/H buffers for current accumulation
-    uint8_t input[64];    // Next input message block
-    uint8_t digest[32];   // Hash result
+    uint64_t size; // Use to accumulate the size of the message (in bytes)
+	uint32_t buffer[8]; // A/B/C/D/E/F/G/H buffers for current accumulation
+	uint8_t input[64]; // Next input message block
+	uint8_t digest[32]; // Hash result
 }	_sha256_context_t;
 
 void _sha256_init(_sha256_context_t *context);
--- srcs/algorithms/sha256/sha256_intern.c
+++ srcs/algorithms/sha256/sha256_intern.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 22:01:27 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/12/06 22:18:07 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/12/06 22:37:04 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,6 @@ static const uint32_t k[64] = {
 	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
 };
 
-static uint32_t rotateLeft(uint32_t x, uint32_t n){
-    return (x << n) | (x >> (32 - n));
-}
-
 static uint32_t rotateRight(uint32_t x, uint32_t n){
 	return (x >> n) | (x << (32 - n));
 }
@@ -61,11 +57,113 @@ void _sha256_init(_sha256_context_t *context)
 	context->buffer[7] = H;
 }
 
-static void sha256_do_a_round(uint32_t *buffer, uint32_t *input)
-{}
+static void sha256_do_a_round(uint32_t *buffer, uint8_t *input)
+{
+	uint32_t tmp_a = buffer[0];
+	uint32_t tmp_b = buffer[1];
+	uint32_t tmp_c = buffer[2];
+	uint32_t tmp_d = buffer[3];
+	uint32_t tmp_e = buffer[4];
+	uint32_t tmp_f = buffer[5];
+	uint32_t tmp_g = buffer[6];
+	uint32_t tmp_h = buffer[7];
+
+	uint32_t m[64];
+
+	int i, j;
+	for (i = 0, j = 0; i < 16; ++i, j += 4)
+		m[i] = (input[j] << 24) | (input[j + 1] << 16) | (input[j + 2] << 8) | (input[j + 3]);
+	for ( ; i < 64; ++i)
+		m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+
+	uint32_t t1, t2;
+	for (i = 0; i < 64; ++i) {
+		t1 = tmp_h + EP1(tmp_e) + CH(tmp_e,tmp_f,tmp_g) + k[i] + m[i];
+		t2 = EP0(tmp_a) + MAJ(tmp_a,tmp_b,tmp_c);
+		tmp_h = tmp_g;
+		tmp_g = tmp_f;
+		tmp_f = tmp_e;
+		tmp_e = tmp_d + t1;
+		tmp_d = tmp_c;
+		tmp_c = tmp_b;
+		tmp_b = tmp_a;
+		tmp_a = t1 + t2;
+	}
+
+	buffer[0] += tmp_a;
+	buffer[1] += tmp_b;
+	buffer[2] += tmp_c;
+	buffer[3] += tmp_d;
+	buffer[4] += tmp_e;
+	buffer[5] += tmp_f;
+	buffer[6] += tmp_g;
+	buffer[7] += tmp_h;
+}
 
 void _sha256_fill_and_run_if_complete(_sha256_context_t *context, uint8_t *input, uint64_t input_len)
-{}
+{
+	uint8_t prepared_input[64];
+
+	unsigned int offset = context->size % 64;
+    context->size += (uint64_t)input_len;
+
+    for(unsigned int i = 0; i < input_len; ++i)
+	{
+        context->input[offset++] = (uint8_t)*(input + i);
+        if(offset % 64 == 0)
+		{
+			for(unsigned int j = 0; j < 64; ++j)
+			{
+				prepared_input[j] = 0;
+				prepared_input[j] |= (uint32_t)(context->input[(j * 4) + 3]) << 24;
+				prepared_input[j] |= (uint32_t)(context->input[(j * 4) + 2]) << 16;
+				prepared_input[j] |= (uint32_t)(context->input[(j * 4) + 1]) <<  8;
+				prepared_input[j] |= (uint32_t)(context->input[(j * 4) + 0]) <<  0;
+			}
+            sha256_do_a_round(context->buffer, prepared_input);
+            offset = 0;
+		}
+	}
+}
 
 void _sha256_padd_and_finalize(_sha256_context_t *context)
-{}
+{
+	uint32_t i = context->size % 64;
+
+	if (context->size < 56) {
+		context->input[i++] = 0x80;
+		while (i < 56)
+			context->input[i++] = 0x00;
+	}
+	else {
+		context->input[i++] = 0x80;
+		while (i < 64)
+			context->input[i++] = 0x00;
+		sha256_do_a_round(context->buffer, context->input);
+		ft_memset(context->input, 0, 56);
+	}
+
+	context->size *= 8;
+	context->input[63] = context->size;
+	context->input[62] = context->size >> 8;
+	context->input[61] = context->size >> 16;
+	context->input[60] = context->size >> 24;
+	context->input[59] = context->size >> 32;
+	context->input[58] = context->size >> 40;
+	context->input[57] = context->size >> 48;
+	context->input[56] = context->size >> 56;
+
+	sha256_do_a_round(context->buffer, context->input);
+
+	for (int i = 0; i < 4; ++i) {
+		context->digest[i] = context->buffer[0] >> (24 - i * 8);
+		context->digest[i + 4] = context->buffer[1] >> (24 - i * 8);
+		context->digest[i + 8] = context->buffer[2] >> (24 - i * 8);
+		context->digest[i + 12] = context->buffer[3] >> (24 - i * 8);
+		context->digest[i + 16] = context->buffer[4] >> (24 - i * 8);
+		context->digest[i + 20] = context->buffer[5] >> (24 - i * 8);
+		context->digest[i + 24] = context->buffer[6] >> (24 - i * 8);
+		context->digest[i + 28] = context->buffer[7] >> (24 - i * 8);
+	}
+
+}
